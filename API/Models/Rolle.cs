ce53e81f739ab8fa8d500b5ebf02c568196ad409
Sherina Eria Hastalina <hastalinas@gmail.com>
2023-07-18 08:49:39 +0700

    models for database

---
+++ API/Models/Rolle.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models;
+
+public class Rolle
+{
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+}
