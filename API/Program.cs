3843d28b0ee91ddd757e9336794ccf0b3bce8246
Sherina Eria Hastalina <hastalinas@gmail.com>
2023-07-20 14:24:45 +0700

    booking

--- API/Program.cs
+++ API/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddScoped<IRoomRepository, RoomRepository>();
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IEducationRepository, EducationRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
