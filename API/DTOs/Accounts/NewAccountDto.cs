222172c05bfe5f90b3295ed516296948c05b2ad9
Sherina Eria Hastalina <hastalinas@gmail.com>
2023-07-21 23:48:57 +0700

    education dto

--- API/DTOs/Accounts/NewAccountDto.cs
+++ API/DTOs/Accounts/NewAccountDto.cs
@@ -3,7 +3,7 @@ namespace API.DTOs.Accounts;
 
 public class NewAccountDto
 {
-    public Guid Guid { get; set; }
+    //public Guid Guid { get; set; }
     public string Password { get; set; }
     public int OTP { get; set; }
     public bool IsUsed { get; set; }
---
+++ API/DTOs/Educations/NewEducationDto.cs
@@ -0,0 +1,35 @@
+﻿using API.Models;
+namespace API.DTOs.Education;
+
+public class NewEducationDto
+{
+    public Guid Guid { get; set; }
+    public string Major { get; set; }
+    public string Degree { get; set; }
+    public float GPA { get; set; }
+    public string UniversityGuid { get; set; }
+
+    public static implicit operator Education(EducationDto educationDto)
+    {
+        return new Education
+        {
+            Guid = educationDto.Guid,
+            Major = educationDto.Major,
+            Degree = educationDto.Degree,
+            GPA = educationDto.GPA,
+            UniversityGuid = educationDto.UniversityGuid
+        };
+    }
+
+    public static explicit operator EducationDto(Education education)
+    {
+        return new EducationDto
+        {
+            Guid = education.Guid,
+            Major = education.Major,
+            Degree = education.Degree,
+            GPA = education.GPA,
+            UniversityGuid = education.UniversityGuid
+        };
+    }
+}
